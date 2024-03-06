3fda59b4f0e17a8c9b27bdeb580936cca8e25bd5
Smokashi23 <sakshimokashi640@gmail.com>
2024-03-06 14:34:46 +0530

    assignmemt2 added

--- src/FetchData.tsx
+++ src/FetchData.tsx
@@ -1,7 +1,23 @@
-import { useEffect } from "react"
+ import { useEffect, useState } from "react"
+import TodoItem from "./models/todoItem";
 
-export default function FetchData(){
-//   const handledata=()=>{
+
+// export default function useFetchData(){
+// //   const handledata=()=>{
+// //   fetch("http://localhost:8000/data")
+// //   .then(res=>{
+// //     console.log(res)
+// //     return res.json()
+// //   })
+// //   .then(data => {
+// //     console.log(data)
+// //   })
+// //   .catch(err=>{
+// //     console.log(err)
+// //   })
+// // }
+
+// useEffect(()=>{
 //   fetch("http://localhost:8000/data")
 //   .then(res=>{
 //     console.log(res)
@@ -13,30 +29,36 @@ export default function FetchData(){
 //   .catch(err=>{
 //     console.log(err)
 //   })
+// },[]
+// )
+
+// console.log("Here")
+//   return(
+//     <div>
+//       <h1>Hello</h1>
+//       {/* <button onClick={handledata}>ADD</button> */}
+//     </div>
+//   )
+
 // }
 
-useEffect(()=>{
-  fetch("http://localhost:8000/data")
-  .then(res=>{
-    console.log(res)
-    return res.json()
-  })
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err=>{
-    console.log(err)
-  })
-},[]
-)
+export default function useFetchData(url:string){
+  const [data, setData] = useState <TodoItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-console.log("Here")
-  return(
-    <div>
-      <h1>Hello</h1>
-      {/* <button onClick={handledata}>ADD</button> */}
-  
-    </div>
-  )
+  useEffect(()=> {
 
+  setTimeout(()=>{
+    fetch(url)
+    .then((res)=> res.json())
+    .then((data)=>{setData(data);setLoading(false);setError(null)})
+    .catch((error:any)=>{setError(error.message)
+      setLoading(false)});
+  },1000)
+  },[]);
+
+  return {data,loading,error}
 }
+
+
